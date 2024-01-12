eed94b2de282624a7176cbaad6cc652964953d70
권순성 <gwonsunseong@gwonsunseong-ui-MacBookPro-2.local>
2024-01-12 17:51:30 +0900

    fix: afterLogin layout 및 라우트 작업

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>HOME 페이지</div>;
+}
--- src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -3,7 +3,6 @@ import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
 export default function Signup() {
   return (
     <>
-      난 가로채기지롱 ㅋㅋ
       <SignupModal />
     </>
   );
